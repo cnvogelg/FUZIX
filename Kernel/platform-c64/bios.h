71c9c87ebca85c7ebae44a34c6f6c6e2f9375cbc
Christian Vogelgsang <chris@vogelgsang.org>
2023-02-10 22:18:59 +0100

    added c64 platform

---
+++ Kernel/platform-c64/bios.h
@@ -0,0 +1,6 @@
+#ifndef BIOS_H
+#define BIOS_H
+
+extern void __fastcall__ cputc(uint8_t c);
+
+#endif
---
+++ Kernel/platform-c64/devtty.c
@@ -0,0 +1,70 @@
+#include <kernel.h>
+#include <kdata.h>
+#include <printf.h>
+#include <stdbool.h>
+#include <devtty.h>
+#include <device.h>
+#include <vt.h>
+#include <tty.h>
+
+#include <bios.h>
+
+static char tbuf1[TTYSIZ];
+PTY_BUFFERS;
+
+struct s_queue ttyinq[NUM_DEV_TTY + 1] = {	/* ttyinq[0] is never used */
+	{NULL, NULL, NULL, 0, 0, 0},
+	{tbuf1, tbuf1, tbuf1, TTYSIZ, 0, TTYSIZ / 2},
+	PTY_QUEUES
+};
+
+tcflag_t termios_mask[NUM_DEV_TTY + 1] = {
+	0,
+	_CSYS	/* TODO */
+};
+
+/* Output for the system console (kprintf etc) */
+void kputchar(uint8_t c)
+{
+	if (c == '\n')
+		tty_putc(1, '\r');
+	tty_putc(1, c);
+}
+
+ttyready_t tty_writeready(uint8_t minor)
+{
+	return TTY_READY_NOW;
+}
+
+void tty_putc(uint8_t minor, unsigned char c)
+{
+	cputc(c);
+}
+
+void tty_setup(uint8_t minor, uint8_t flag)
+{
+}
+
+void tty_sleeping(uint8_t minor)
+{
+}
+
+/* For the moment */
+int tty_carrier(uint8_t minor)
+{
+	return 1;
+}
+
+void tty_data_consumed(uint8_t minor)
+{
+}
+
+void tty_poll(void)
+{
+        uint8_t x=0;
+        
+        if (x) {
+		tty_inproc(1, x);
+	}
+}
+                
